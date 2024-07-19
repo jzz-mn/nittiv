7fdb1866c0b89e9b71ccacf8f44640357be416c0
Jazzmin Perez <perez.jazzmin.h@gmail.com>
2024-07-19 13:38:44 +0800

    firebase setup

--- nittiv_application/lib/main.dart
+++ nittiv_application/lib/main.dart
@@ -1,6 +1,23 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: FirebaseOptions(
+            apiKey: "AIzaSyBE8ok9GKfWK5AH_8ADEcM7RgQGp4lyY8U",
+            authDomain: "nittiv-project-2024.firebaseapp.com",
+            projectId: "nittiv-project-2024",
+            storageBucket: "nittiv-project-2024.appspot.com",
+            messagingSenderId: "85086430378",
+            appId: "1:85086430378:web:b35dae13b00d2a4d275cd3"));
+  } else {
+    await Firebase.initializeApp();
+  }
+
   runApp(const MyApp());
 }
 
